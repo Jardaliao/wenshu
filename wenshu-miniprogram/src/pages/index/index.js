34f576a7cce74852427cb989dfe7af481624d31e
Jarda <jardaliao@gmail.com>
2023-12-26 00:03:47 +0800

    login接口模拟成功

--- wenshu-miniprogram/src/pages/index/index.js
+++ wenshu-miniprogram/src/pages/index/index.js
@@ -11,7 +11,7 @@ Page({
       },
       data: {
         username: "17674663791",
-        password: encrypt("Lj@4536251"),
+        password: encodeURIComponent(encrypt("Lj@4536251")),
         // password: "S4xs0Pi%2Bjjm%2Fl0K%2FS1ZYdj0av8LK9PYrZyVRPLvc74d2Cgt4GK5mv82AieavX2eYH596CWM14GzaNlCxE71W87hStwbXnpApEvVHryW9%2B%2FK0R0F5PrdqcjwxaWDCuWNXmHVbIds9EBqfabIs7QBiAev0BE2KnLZ%2FzD33rE0jHW7dS1%2FLWmJTbuk7hEkqLCkzN48upemlNRSoDzDLHTuIDPks3CVUZMxRQiJyTpXePVkAhMmqiTO648Jv5NWkEhGQe3ejFMJmdZe74xZl0tD1lcqqjvfQ%2BLQSTZzmBDctkR2hlGfJfahj254Y%2FRAeoS2rIrOtxkd04tnbX0lhTRyhLA%3D%3D",
         appDomain: "wenshu.court.gov.cn"
       }
--- wenshu-miniprogram/src/utils/login.js
+++ wenshu-miniprogram/src/utils/login.js
@@ -3,9 +3,7 @@ import jsencrypt from '../utils/custom_jsencrypt';
 export const LOGIN_URL = "https://account.court.gov.cn/api/login"
 const PUBKEY = `MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5GVku07yXCndaMS1evPIPyWwhbdWMVRqL4qg4OsKbzyTGmV4YkG8H0hwwrFLuPhqC5tL136aaizuL/lN5DRRbePct6syILOLLCBJ5J5rQyGr00l1zQvdNKYp4tT5EFlqw8tlPkibcsd5Ecc8sTYa77HxNeIa6DRuObC5H9t85ALJyDVZC3Y4ES/u61Q7LDnB3kG9MnXJsJiQxm1pLkE7Zfxy29d5JaXbbfwhCDSjE4+dUQoq2MVIt2qVjZSo5Hd/bAFGU1Lmc7GkFeLiLjNTOfECF52ms/dks92Wx/glfRuK4h/fcxtGB4Q2VXu5k68e/2uojs6jnFsMKVe+FVUDkQIDAQAB`
 
-// console.log(jsencrypt)
-
-const encryptor = new jsencrypt()
+const encryptor = new jsencrypt.JSEncrypt()
 encryptor.setPublicKey(PUBKEY)
 
 export function encrypt(str) {
