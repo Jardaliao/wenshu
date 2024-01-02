d94e65f81a517f7ee238b183a039a0145d3b8376
Liao Jie <jie.liao@coolkit.cn>
2024-01-02 15:33:11 +0800

    wenshu-backend服务接入systemd打包

---
+++ wenshu-backend/deb/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 启动服务
+exec ./wenshu.bin &
+
+# 启动服务的返回值
+retval=$?
+
+# 服务的pid
+pid=$!
+
+# 如果返回值非0，失败退出
+[ $retval -eq 0 ] || exit $retval
+
+# pid空，失败退出，否则成功退出
+if ! ps -p $pid > /dev/null ; then
+  exit 1
+fi
+exit 0
