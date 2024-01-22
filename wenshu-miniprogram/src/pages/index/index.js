cdd6cd9de1724ac54e2ed158748885d91e71767b
Jarda <jardaliao@gmail.com>
2024-01-22 23:31:46 +0800

    搜索页面增加查询进度

--- wenshu-miniprogram/src/pages/index/index.js
+++ wenshu-miniprogram/src/pages/index/index.js
@@ -21,7 +21,7 @@ Page({
 
     await currentUser({ pageId, requestToken, extra: {} }) // 保持Session？
 
-    const titles = ['首页', '外卖', '商超生鲜', '购物', '美食饮品', '生活服务', '休闲娱乐', '出行']
+    const titles = ['全国判决书查询']
     const tabs = titles.map(item => ({ title: item }))
     this.setData({ tabs })
 
