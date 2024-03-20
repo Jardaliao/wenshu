2092abf76c2ae80bd5639b79e4ce7aebc3cf80fe
Jarda <jardaliao@gmail.com>
2024-03-20 23:51:06 +0800

    列表渲染

--- wenshu-web/src/main.js
+++ wenshu-web/src/main.js
@@ -5,7 +5,7 @@ import {
     Sticky, PickerGroup, Picker, DatePicker, Popup,
     RadioGroup, Radio, CheckboxGroup, Checkbox,
     Icon, ConfigProvider, Overlay, Loading, DropdownMenu,
-    DropdownItem
+    DropdownItem, List, TextEllipsis, Tag
 } from 'vant'
 
 import App from './App.vue'
@@ -24,7 +24,7 @@ const requiredVant = [RadioGroup, Radio, Form, Row, Col, Button,
     Sticky, Search, NavBar, Field, CellGroup, Cell, TreeSelect,
     Dialog, PickerGroup, DatePicker, Popup, CheckboxGroup, Checkbox,
     Picker, Icon, ConfigProvider, Overlay, Loading, DropdownMenu,
-    DropdownItem];
+    DropdownItem, List, TextEllipsis, Tag];
 for (let v of requiredVant) { app.use(v) }
 
 app.mount('#app')
